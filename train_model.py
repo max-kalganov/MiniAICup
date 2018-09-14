450eb311aea5b7e39c89de63415f0b75a7eb38d1
max-kalganov <maksim.kalganov@gmail.com>
2018-09-14 20:43:15 +0300

    complete part where we are using model

--- train_model.py
+++ train_model.py
@@ -1,35 +1,52 @@
-import json
 from keras.models import Sequential
 from keras.layers import Dense
-import numpy
+import numpy as np
+from keras.utils import np_utils
+from sklearn.preprocessing import LabelEncoder
 
-numpy.random.seed(2)
+
+
+
+np.random.seed(2)
 
 def reading_json(filename):
-    fileobj = open(filename, "r")
-    json_string = ""
-    if fileobj.mode == "r":
-        json_string = fileobj.read()
-    match_params = json.loads(json_string)
-    return match_params
+    # TODO: напиши здесь плиз часть про чтение json файла
+    match_params = []
+    match_moves = []
+    return match_params, match_moves
 
-def makeTrainAndTestSets(match_params_dict):
+def makeTrainAndTestSets(match_params, match_moves):
+    # TODO: а здесь перевод их в numpy. формат такой : match_params_train, match_params_test - (количество тиков, количество параметров)
+    # TODO: а формат match_moves_train, match_moves_test - столбец ходов.(не меняй названия движений на цифры. у меня эот есть)
     match_params_train = []
     match_params_test = []
-    return match_params_train, match_params_test
+    match_moves_train = []
+    match_moves_test = []
+    return match_params_train, match_params_test, match_moves_train, match_moves_test
 
-# сюда нужно подавать в параметры сет в котором столбцы - каждый тик и параметры для него: все параметры, кото
-# рые даются в начале этого матча и все параметры за данный тик.
+
+def format_answerSet(answerSet):
+    encoder = LabelEncoder()
+    encoder.fit(answerSet)
+    encoded_y = encoder.transform(answerSet)
+    dummy_y = np_utils.to_categorical(encoded_y)
+    print("encoded answerSet = ", dummy_y)
+    return dummy_y
+
 
 def train(match_params, correct_decision):
+    # format answerSet
+    dummy_y = format_answerSet(correct_decision)
+
     model = Sequential()
-    num_of_params, num_of_ticks = match_params.shape
+    set_size, num_of_params = match_params.shape
+
     model.add(Dense(num_of_params, input_dim=num_of_params, activation='relu'))
     model.add(Dense(num_of_params, activation='relu'))
     model.add(Dense(num_of_params, activation='relu'))
-    model.add(Dense(3, activation='sigmoid'))
-    model.compile()
-    model.fit()
+    model.add(Dense(3, activation='softmax'))
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
+    model.fit(match_params, dummy_y, epochs=500, batch_size=2)
 
     scores = model.evaluate()
     print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
@@ -38,10 +55,17 @@ def train(match_params, correct_decision):
 
 def main():
     filename = "train.json"
-    match_params_dict = reading_json(filename)
-    match_params_train, match_params_test = makeTrainAndTestSets(match_params_dict)
+    match_params, match_moves = reading_json(filename)
+    match_params_train, match_params_test, match_moves_train, match_moves_test = makeTrainAndTestSets(match_params, match_moves)
 
     # TODO: нужно ещё нормализовать данные
-    model = train(match_params_train)
+    model = train(match_params_train, match_moves_train)
+    # TODO: нужно проследить, что при предсказании мы получаем 0='left' 1='right' 2='stop'
+    dummy_y = format_answerSet(match_moves_test)
+    scores = model.evaluate(match_params_test, dummy_y)
+    print("checking accurance of the testSet:")
+    print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
 
     #model.predict(...)
+    model.save('my_model.h5')  # creates a HDF5 file 'my_model.h5'
+    del model
