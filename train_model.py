ea2827dfd041e7b491ad0a730590ffe835b65a61
max-kalganov <maksim.kalganov@gmail.com>
2018-09-14 11:42:52 +0300

    write part of the train function. it will be done after enough number of tests in the test_keras file.

--- train_model.py
+++ train_model.py
@@ -18,13 +18,30 @@ def makeTrainAndTestSets(match_params_dict):
     match_params_test = []
     return match_params_train, match_params_test
 
-def train(match_params):
+# сюда нужно подавать в параметры сет в котором столбцы - каждый тик и параметры для него: все параметры, кото
+# рые даются в начале этого матча и все параметры за данный тик.
 
+def train(match_params, correct_decision):
+    model = Sequential()
+    num_of_params, num_of_ticks = match_params.shape
+    model.add(Dense(num_of_params, input_dim=num_of_params, activation='relu'))
+    model.add(Dense(num_of_params, activation='relu'))
+    model.add(Dense(num_of_params, activation='relu'))
+    model.add(Dense(3, activation='sigmoid'))
+    model.compile()
+    model.fit()
+
+    scores = model.evaluate()
+    print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+    return model
 
 
 def main():
     filename = "train.json"
     match_params_dict = reading_json(filename)
     match_params_train, match_params_test = makeTrainAndTestSets(match_params_dict)
+
+    # TODO: нужно ещё нормализовать данные
     model = train(match_params_train)
 
+    #model.predict(...)
