a8c931c6ab49a8629af3009e57a5e8d01a79e7b3
mr_neither <mr.neitherman@gmail.com>
2018-09-15 17:58:56 +0300

    jsonnumpy

--- jsonnumpy.py
+++ jsonnumpy.py
@@ -18,31 +18,37 @@ def getparams_TypeTick(params):
 
 def getparams_NewMatch(data):
     l = []
-    for item in data:
-        for seq in data[item]:
-            # l.append(data[item][seq])
+    for item in sorted(data):
+        for seq in sorted(data[item]):
             if seq == 'button_poly' or seq == 'car_body_poly':
                 for pos in data[item][seq]:
                     l.append(pos)
-            if seq == 'front_wheel_position' or seq == 'rear_wheel_position' or seq == 'rear_wheel_joint':
+            elif seq == 'front_wheel_position' or seq == 'rear_wheel_position' or seq == 'rear_wheel_joint':
                 l.append(data[item][seq])
-            if seq == 'rear_wheel_damp_position' or seq == 'front_wheel_damp_position':
+            elif seq == 'rear_wheel_damp_position' or seq == 'front_wheel_damp_position':
                 l.append(data[item][seq])
-            if seq == 'segments':
+            elif seq == 'segments':
                 for pos in data[item][seq]:
                         l.append(np.hstack(pos))
+            else:
+                l.append(data[item][seq])
     return l
 
 
 
 parsed_data = json.loads(json_data)
 ticks = []
-new_match_info=[]
+new_match_info = []
+'''
+Обрати внимание на отображение данных типа 'new_match'. Оно почему-то разные данные показывает
+'''
 
 for data in parsed_data['visio_info']:
     if data['type'] == 'tick':
             ticks.append(np.hstack(getparams_TypeTick(data['params'])))
     if data['type'] == 'new_match':
             new_match_info.append(np.hstack(getparams_NewMatch(data['params'])))
+            print(data['params'])
 
-print(new_match_info[0])
+for i in new_match_info:
+    print(i.shape)
